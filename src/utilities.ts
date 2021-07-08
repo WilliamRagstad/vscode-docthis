53e5ef72f7dfaffde75de6245f37055845230bcb
William Rågstad <william.ragstad@gmail.com>
2021-07-08 10:40:32 +0200

    Fix spacing on empty line
    Now indent spaces will not be added for empty lines, but because tabstops are $1, $2, etc strings, these will be treated as non-empty line and should therefore be indented.

--- src/utilities.ts
+++ src/utilities.ts
@@ -253,8 +253,7 @@ export class SnippetStringBuilder {
                     return;
                 }
                 sb.append(" *");
-                const trimmed = line.trim().replace(/ /g, "");
-                if (trimmed !== "" && trimmed !== " ") {
+                if (line.trim() !== "") {
                     sb.append(" ");
                 }
                 sb.appendLine(line);
